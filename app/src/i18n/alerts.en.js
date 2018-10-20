07940b0bd15262a748e3e48c064db85e553d9ff1
Raul Ferriz <rferriz@gamehouse.com>
2018-10-20 20:06:32 +0200

    Translations

--- app/src/i18n/alerts.en.js
+++ app/src/i18n/alerts.en.js
@@ -1,5 +1,10 @@
 
 
 export default {
-  title_flooding_alert : 'Flooding Alert'
+  title_flooding_alert : 'Flooding Alert',
+  title_heatwave_alert: 'Heat wave Alert',
+  title_rain_alert: 'Rain Alert',
+  title_wind_alert: 'Strong Wind Alert',
+  title_avalanche_alert: 'Avalche Alert',
+  title_eruption_alert: 'Volcanic Eruption Alert'
 }
