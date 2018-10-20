b6694931883b22db8827b2896d488af9bcb773c0
Raul Ferriz <rferriz@gamehouse.com>
2018-10-20 18:55:14 +0200

    Error processing

--- backend/src/controller/alerts.js
+++ backend/src/controller/alerts.js
@@ -191,39 +191,45 @@ module.exports = function() {
     const lat = req.params.lat;
     const long = req.params.long;
 
-    const resolved = await geocoder.reverse(lat, long);
-    const autcom = await geocoder.administrativeLevel(resolved[0]);
-    if (!autcom) {
-      console.error('Unable to get administrative level from', resolved[0]);
-      // Return error
+    try {
+      const resolved = await geocoder.reverse(lat, long);
+      const autcom = await geocoder.administrativeLevel(resolved[0]);
+      if (!autcom) {
+        console.error('Unable to get administrative level from', resolved[0]);
+        // Return error
+      }
+
+      const autcomToAEMETCode = {
+        'España': 'esp',
+        'Andalucía': 61,
+        'Aragón': 62,
+        'Asturias, Principado de': 63,
+        'Ballears, Illes': 64,
+        'Ceuta': 78,
+        'Canarias': 65,
+        'Cantabria': 66,
+        'Castilla y León': 67,
+        'Castilla - La Mancha': 68,
+        'Cataluña': 69,
+        'Comunitat Valenciana': 77,
+        'Extremadura': 70,
+        'Galicia': 71,
+        'Madrid, Comunidad de': 72,
+        'Melilla': 79,
+        'Murcia, Región de': 73,
+        'Navarra, Comunidad Foral de': 74,
+        'País Vasco': 75,
+        'Rioja, La': 76
+      }
+
+      const alerts = await getAlerts(autcomToAEMETCode[autcom], {lat: lat, lon: long});
+
+      return res.json(alerts).end();
+    } catch (err) {
+      return res.status(400).json({
+        error: err.message
+      });
     }
-
-    const autcomToAEMETCode = {
-      'España': 'esp',
-      'Andalucía': 61,
-      'Aragón': 62,
-      'Asturias, Principado de': 63,
-      'Ballears, Illes': 64,
-      'Ceuta': 78,
-      'Canarias': 65,
-      'Cantabria': 66,
-      'Castilla y León': 67,
-      'Castilla - La Mancha': 68,
-      'Cataluña': 69,
-      'Comunitat Valenciana': 77,
-      'Extremadura': 70,
-      'Galicia': 71,
-      'Madrid, Comunidad de': 72,
-      'Melilla': 79,
-      'Murcia, Región de': 73,
-      'Navarra, Comunidad Foral de': 74,
-      'País Vasco': 75,
-      'Rioja, La': 76
-    }
-
-    const alerts = await getAlerts(autcomToAEMETCode[autcom], {lat: lat, lon: long});
-
-    return res.json(alerts).end();
   }
 
   return {
