df930a252f812c4224397fdd56fa9e4e25d97f1a
biniesta <belen.iniesta@live.com>
2018-10-20 20:05:37 +0200

    fixing the ui for techiniques

--- app/src/views/Technique.vue
+++ app/src/views/Technique.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
   v-card
     v-img(src='https://2.bp.blogspot.com/-bUGWAPSSs-s/Vup0oMttlDI/AAAAAAAABbk/Axz1k4C0FEspjyYvsPwtEAfxYLcauZDGg/w1200-h630-p-k-no-nu/maniobra%2Bde%2Bheimlich.jpg', height='200px')
-    v-card-title(primary-title='')
-      div
-        .headline Maniobra de Heimlich
     
-      iframe(width='560', height='315', src='https://www.youtube.com/embed/PNTP8VKGou8', frameborder='0', allow='autoplay; encrypted-media', allowfullscreen='')
+    v-card-title(primary-title='')
+      div(class="headline") Heimlich maneuver
+      
+    .video__container()
+      iframe(width='560', height='315', src='https://www.youtube.com/embed/7CgtIgSyAiU', frameborder='0', allow='autoplay; encrypted-media', allowfullscreen='' style="display: block;margin: 0 auto; max-width: 80%;")
+
     
 </template>
 
